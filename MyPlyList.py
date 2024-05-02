212d9c35e28a59170e18c9238e174e911ea1c707
Sulaiman Alharbi <Sulaiman Alharbi>
2024-05-02 22:23:01 +0300

    added MyPlayList

---
+++ MyPlyList.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+def serachByname(name, musicList):
+    for song in musicList:
+        if song['track_name'] == name:
+            return song
+    
+    return None
+
+def maxKey(musicList, keyname):
+    maxKey = {}
+    for song in musicList:
+        key = song.get(keyname)
+
+        if key:
+            maxKey[key] =maxKey.get(key, 0) + 1
+    
+    return max(maxKey, key = maxKey.get)
+
+#===========================================================
+
+musicLib = pd.read_csv('tcc_ceds_music.csv')
+
+testadd = musicLib[musicLib['track_name'] == 'let her cry']
+testadd2 = musicLib[musicLib['track_name'] == 'the way you look tonight']
+
+
+myPlayList = []
+myPlayList.extend(testadd.to_dict(orient='records'))
+myPlayList.extend(testadd.to_dict(orient='records'))
+myPlayList.extend(testadd.to_dict(orient='records'))
+myPlayList.extend(testadd2.to_dict(orient='records'))
+
+print(myPlayList[0])
+for song in myPlayList:
+    print('song name:', song['track_name'])
+
+
+
